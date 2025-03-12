8ba76c551da658dbf506fa609d20da982dfa1191
Sam Feng <fengyuan20000303@gmail.com>
2025-03-11 23:11:25 -0700

    Update Fish.js

--- docs/Fish.js
+++ docs/Fish.js
@@ -32,7 +32,7 @@ class Fish {
 
         // Create the fish sprite
         this.sprite = scene.add.sprite(x, y, `fish_${this.id}`);
-        this.sprite.setScale(0.5);
+        this.sprite.setScale(0.75);
         
         // functions
         this.setRandomDirection();
